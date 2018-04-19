64b6839e94238eb8bedb09791769bc7bbd892104
Shivaji Reddy <shivajireddy.d@gmail.com>
2018-04-19 20:41:51 +0530

    simple_lang

---
+++ HackerRank/sipmle_lang.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int n; 
+	cin >> n;
+	string t[n];
+	long y[n], x = 0;
+
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> t[i];
+		cin >> y[i];
+	}
+
+	for (int i = 0; i < n; ++i)
+	{
+		if(s[i][0] == 's' && y[i] > x)
+			x = y[i];
+
+		if(s[i][0] == 'a' && x + y[i] > x)
+			x += y[i];
+	}
+
+	cout << x << endl;
+
+
+	return 0;
+}
