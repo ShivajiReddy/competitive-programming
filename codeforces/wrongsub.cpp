d0f21ae0da20f89da9405bf99e66cea17dd00e00
Shivaji Reddy <shivajireddy.d@gmail.com>
2018-05-11 22:35:40 +0530

    codeforces...

--- codeforces/wrongsub.cpp
+++ codeforces/wrongsub.cpp
@@ -1,17 +1,17 @@
-#include <bits/stdc++.h>
-using namespace std;
+	#include <bits/stdc++.h>
+	using namespace std;
 
-int main(int argc, char const *argv[])
-{ 
-	int n, k;
-	cin >> n >> k;
-	for (int i = 0; i < k; ++i)
-	{
-		if(n%10 == 0)	n = n/10;
-		else n = n-1;
+	int main(int argc, char const *argv[])
+	{ 
+		int n, k;
+		cin >> n >> k;
+		for (int i = 0; i < k; ++i)
+		{
+			if(n%10 == 0)	n = n/10;
+			else n = n-1;
+		}
+		cout << n << endl;
+		
+
+		return 0;
 	}
-	cout << n << endl;
-	
-
-	return 0;
-}
