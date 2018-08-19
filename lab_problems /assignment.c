a204f859c775c9789c892ea395fa8ac77ef6ae3c
Shivaji Reddy <shivajireddy.d@gmail.com>
2018-08-20 00:31:43 +0530

    added lab_codes folder

---
+++ lab_problems /assignment.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	int n;
+	scanf("%d", &n);
+	for(int i = 0; i < n; i++)
+	{ 
+		scanf("%d", &a[i]); 
+	}
+	
+
+	return 0;
+}
---
+++ lab_problems /dfs.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int n, adj_graph[4][4], visited[10]; 
+
+void dfs(int i)
+{
+    int j;
+    printf("\n%d", i+1);
+    visited[i]=1;
+    
+    for(j=0; j<n; j++)
+       if(!visited[j] && adj_graph[i][j]==1)
+            dfs(j);
+} 
+
+void main()
+{
+    int i,j;
+    printf("Enter number of vertices:\n");
+   
+    scanf("%d",&n);
+ 
+    
+    printf("\nEnter adjecency matrix: \n");
+   
+    for(i=0; i<n; i++)
+    {
+    	for(j=0; j<n; j++)
+    	{
+    		scanf("%d", &adj_graph[i][j]);
+    	}
+    }   
+  
+    for(i=0; i<n; i++)
+        visited[i]=0;
+ 
+    dfs(0);
+}
+ 
