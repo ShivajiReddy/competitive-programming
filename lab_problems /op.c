84d0043c427a476adf3f887bce2b0216b77e6e28
Shivaji Reddy <shivajireddy.d@gmail.com>
2018-11-09 12:19:00 +0530

    added os

---
+++ lab_problems /op.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int ispresent(int a,int s,int st[])
+{
+	int i;
+	for(i=0;i<s;i++)
+	{
+		if(a==st[i])
+			return i;
+	}
+
+	return -1;
+}
+
+int getindex(int sz,int f[],int n,int p[],int pos)
+{
+	int rep = 0, max = pos, i ,j;
+
+	for (i = 0; i < sz; ++i)
+	{
+		for (j = pos; j >= 0; j--)
+		{
+			if(f[i]==p[j])
+			{
+				if(j<max)
+				{
+					max=j;
+					rep=i;
+				}
+				break;
+			}
+		}
+		if(j == 0) return i;
+	}
+
+	return rep;
+
+	//return occ[0]>occ[1]?(occ[0]>occ[2]?occ[0]:occ[2]):(occ[1]>occ[2]?occ[1]:occ[2]);	
+}
+
+/*
+20
+3
+7 0 1 2 0 3 0 4 2 3 0 3 2 1 2 0 1 7 0 1*/
+
+void op(int n,int sz,int p[])
+{
+	int f[sz];
+	int pg = sz;
+	for(int i=0;i<sz;i++)
+	{
+		f[i]=p[i];
+	}
+	
+	for(int i=sz;i<n;i++)
+	{
+		if(ispresent(p[i],sz,f)==-1)
+		{
+
+			int ptr = getindex(sz,f,n,p,i);
+			for(int j=0;j<sz;j++)
+				printf("%d ",f[j]);
+
+			printf(" %d replacing with %d ", f[ptr], p[i]);
+			printf("\n");
+			//printf("%d is not present. It's ptr is %d\n", p[i], ptr);			
+			f[ptr] = p[i];
+			pg++;			
+		}
+
+	}
+	for (int i = 0; i < sz; ++i)
+	{
+		printf("%d ", f[i]);
+	}
+	printf("\nPage faults: %d\n",pg );
+}
+
+int main()
+{
+	int sz,n;
+	scanf("%d",&n);
+	scanf("%d",&sz);
+	int p[n];
+	for(int i=0;i<n;i++)
+		scanf("%d",&p[i]);
+	op(n,sz,p);
+}
---
+++ lab_problems /second_chance.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+int isPresent(int p,int lrustack[],int n)
+{
+	int i;
+	for(i=0;i<n;i++)
+	{
+		if(lrustack[i]==p)
+			return i;
+	}
+	return -1;
+}
+
+void lru_secondchance(int p[],int n,int fm_sz)
+{
+	printf("Entering Second Chance Algo\n");
+	int f[fm_sz];
+	int ptr=0;
+	int i,j,k,page_fault=0;
+	for(i=0;i<fm_sz;i++)
+		f[i]=-1;
+	int ref_bit[fm_sz];
+	for(i=0;i<fm_sz;i++)
+		ref_bit[i]=0;
+
+
+	for(i=0;i<n;i++)
+	{
+		if(isPresent(p[i],f,fm_sz)==-1)
+		{
+			while(ref_bit[ptr]==1)
+			{
+				ref_bit[ptr]=0;
+				ptr=(ptr+1)%fm_sz;
+			}
+			f[ptr]=p[i];
+			ref_bit[ptr]=1;
+			ptr=(ptr+1)%fm_sz;
+			page_fault++;
+			printf("Page fault occurred for page: %d\n",p[i]);
+		}
+		else
+		{
+			int loc = isPresent(p[i],f,fm_sz);
+			ref_bit[loc]=1;
+		}
+		printf("FRAME CONTENTS & REF BITS:\n");
+		for(k=0;k<fm_sz;k++)
+			printf("%d\t%d\n",f[k],ref_bit[k]);
+	}
+	printf("PAGE FAULTS: %d\n",page_fault );
+}
+
+
+
+int main()
+{
+	int n,i,j,k,fm_sz;
+	printf("Enter the no.of entries:\n");
+	scanf("%d",&n);
+	int p[n];
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&p[i]);
+	}
+	printf("Enter the frame size:\n");
+	scanf("%d",&fm_sz);
+	lru_secondchance(p,n,fm_sz);
+	return 0;
+}
