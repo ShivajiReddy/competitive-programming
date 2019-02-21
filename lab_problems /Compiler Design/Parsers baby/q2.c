d3969f393a0c44a2c6a98fd59d7b83602f504f67
ShivajiReddy <shivajireddy.d@gmail.com>
2019-02-22 00:30:30 +0530

    parser fr */+/id grammar

--- lab_problems /Compiler Design/Parsers baby/q2.c
+++ lab_problems /Compiler Design/Parsers baby/q2.c
@@ -1,5 +1,4 @@
-//Minor tweaks to Avikantz's code
-
+//Minor tweaks to Original Gangster's code
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
@@ -8,10 +7,9 @@
 
 #define BUFFER_SIZE 256
 
+#define LOG_CURRENT printf("--> %s: %s\n", __PRETTY_FUNCTION__, input[i]);
 #define SUCCESS_HANDLER printf("%s(): Success.\n", __PRETTY_FUNCTION__); exit(1);
 #define ERROR_HANDLER printf("%s(): Error occured.\n", __PRETTY_FUNCTION__); exit(1);
-
-#define LOG_CURRENT printf("--> %s: %s\n", __PRETTY_FUNCTION__, input[i]);
 
 const char delimiters[] = " ";
 
@@ -27,61 +25,76 @@ void T ();
 void T1();
 void F ();
 
-void E () {
+void E () 
+{
 	T();
 	E1();
-	if (strcmp(input[i], "$") == 0) {
+	if (strcmp(input[i], "$") == 0) 
+	{
 		SUCCESS_HANDLER;
-	} else {
+	} else 
+	{
 		ERROR_HANDLER;
 	}
 }
 
-void E1 () {
+void E1 () 
+{
 	LOG_CURRENT;
-	if (strcmp(input[i], "+") == 0) {
+	if (strcmp(input[i], "+") == 0) 
+	{
 		i += 1;
 		T();
 		E1();
-	} else {
+	} else 
+	{
 		return;
 	}
 }
 
-void T () {
+void T () 
+{
 	LOG_CURRENT;
 	F();
 	T1();
 }
 
-void T1 () {
+void T1 () 
+{
 	LOG_CURRENT;
-	if (strcmp(input[i], "*") == 0) {
+	if (strcmp(input[i], "*") == 0) 
+	{
 		i += 1;
 		F();
 		T1();
-	} else {
+	} else 
+	{
 		return;
 	}
 }
 
-void F () {
+void F () 
+{
 	LOG_CURRENT;
-	if (strcmp(input[i], "(") == 0) {
+	if (strcmp(input[i], "(") == 0) 
+	{
 		i += 1;
 		E();
-		if (strcmp(input[i], ")") == 0) {
+		if (strcmp(input[i], ")") == 0) 
+		{
 			i += 1;
 		}
-	} else if (strcmp(input[i], "id") == 0) {
+	} else if (strcmp(input[i], "id") == 0) 
+	{
 		i += 1;
-	} else {
+	} else 
+	{
 		ERROR_HANDLER;
 	}
 }
 
-void parse_input (char *inname) {
-
+void parse_input (char *inname) 
+{
 	strcpy(buffer, inname);
 
 	// Make a temp copy of the string
@@ -90,10 +103,12 @@ void parse_input (char *inname) {
 
 	char *token = (char *)malloc(256 * sizeof(char));
 
-	do {
+	do 
+	{
 		// strsep - extract token from string, returns null if token is not found.
 		token = strsep(&cp, delimiters);
-		if (token != NULL && strlen(token) > 0) {
+		if (token != NULL && strlen(token) > 0) 
+		{
 			strcpy(input[k++], token);
 			// printf("%s || ", input[k-1]);
 		}
@@ -105,7 +120,8 @@ void parse_input (char *inname) {
 
 }
 
-int main (int argc, char const *argv[]) {
+int main (int argc, char const *argv[]) 
+{
 	
 	printf("Enter the input string: \n");
     scanf("%s", inname);
