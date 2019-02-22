8107c364d8edc1951a7b54b1e0a2b2fabc63230c
Shivaji Reddy <shivajireddy.d@gmail.com>
2019-02-22 07:55:41 +0530

    Update q1.c

--- lab_problems /Compiler Design/Parsers baby/q1.c
+++ lab_problems /Compiler Design/Parsers baby/q1.c
@@ -1,5 +1,81 @@
+//Sanjeevi's; minor errors?
 #include <stdio.h>
-#include <stdlib.h>
-#include <stddef.h>
-#include <ctype.h>
 #include <string.h>
+#include <stdlib.h>
+
+char inp[100], prods[100][100];
+int i = 0;
+
+void S();
+void T();
+void Td();
+
+
+void S() 
+{
+    if(inp[i] == 'a') 
+    {
+        printf("Matched: %c\n", inp[i]);
+        ++i;
+        
+    } 
+    else if(inp[i] == '>') 
+    {
+        printf("matched at: %c\n", inp[i]);
+        ++i;
+    }
+    else if(inp[i] == '(') 
+    {
+        printf("matched at %c\n",inp[i]);
+        ++i;
+        T();
+        if(inp[i] == ')')
+        {
+            printf("matched at %c\n",inp[i] );
+            ++i;
+        }
+        else
+            printf("error\n");
+    }
+    else
+        printf("error\n");
+    if(inp[i] == '$')
+    {
+        
+        printf("Parsing complete.");
+        exit(0);
+    
+    }
+}
+
+void T() 
+{
+    S();
+    Td();
+}
+
+void Td() 
+{
+    if(inp[i]==',') 
+    {
+        printf("Matched: %c\n", inp[i]);
+        ++i;
+        S();
+        Td();
+    } 
+}
+
+
+int main() 
+{
+    
+    printf("Enter the input string: \n");
+    scanf("%s", inp);
+
+    int len = strlen(inp);
+    inp[len] = '$';
+
+    S();
+
+    return 0;
+}
