6612b3ef0be176f6c29bb84d17a3277d50f105f7
ShivajiReddy <shivajireddy.d@gmail.com>
2019-03-14 13:38:53 +0530

    takes care of the arguements

--- auto_push.sh
+++ auto_push.sh
@@ -1,8 +1,17 @@
-#Add the files
-git add .
+if [ "$#" -ne 1 ];
+then
+	echo $#
+	echo "Commit message as arg[1]"
+	exit -1
+else
+	#Add the files
+	git add .
 
-#Commit to local repository
-git commit -m "$1"
+	#Commit to local repository
+	git commit -m "$1"
 
-#Push to the remote repository
-git push origin master
+	#Push to the remote repository
+	git push origin master
+	
+	exit 0
+fi
