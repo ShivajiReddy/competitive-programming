9d6d6a9237c429341a29732bf23f260ae268bf04
ShivajiReddy <shivajireddy.d@gmail.com>
2021-12-31 19:56:20 -0800

    min max stocks

---
+++ Leetcode/minMaxStocks.cpp
@@ -0,0 +1,18 @@
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock/submissions/
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minimum = prices[0], maxP = 0;
+        
+        for(int i = 1; i < prices.size(); i++) {
+            if(prices[i] > prices[i-1]) {
+                maxP = max(maxP, prices[i]-minimum);
+            }
+            else {
+                minimum = min(minimum, prices[i]);
+            }
+        }
+        return maxP;
+    }
+};
