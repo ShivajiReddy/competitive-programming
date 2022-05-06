9e01ca3b175fd8901858521b8e1ef9fd2d0b8580
ShivajiReddy <shivajireddy.d@gmail.com>
2022-05-05 23:40:44 -0700

    majority

---
+++ Leetcode/majority_ele_169.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        return nums[nums.size()/2];
+        
+        //have to implement Boyre_Moore for O(n) O(1)
+    }
+    
+};
