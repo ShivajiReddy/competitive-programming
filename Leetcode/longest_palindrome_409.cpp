248d085217dfdadfd01adaea6343e239ecef0093
ShivajiReddy <shivajireddy.d@gmail.com>
2022-05-17 23:56:32 -0700

    409

---
+++ Leetcode/longest_palindrome_409.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        vector <int> freq(128, 0);
+        for(char c : s) {
+            freq[c]++;
+        }
+        
+        int odd_freq = 0;
+        
+        for(int i : freq) {
+            if(i%2 == 1) {
+                odd_freq++;
+            }
+        }
+        return s.size() - odd_freq + (odd_freq > 0);
+    }
+};
