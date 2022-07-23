7351c945f0d981ebfe68c02782dbc6205a9941a5
ShivajiReddy <shivajireddy.d@gmail.com>
2022-07-23 15:13:09 -0700

    number of islands

---
+++ Leetcode/number_of_islands_200.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int count = 0;
+        
+        for(int i = 0; i < grid.size(); i++) {
+            for(int j = 0; j < grid[i].size(); j++) {
+                if(grid[i][j] == '1'){
+                    count++;
+                    dfs(i, j, grid);
+                }
+            }
+        }
+        
+        return count;
+    }
+    
+    void dfs(int i, int j, vector<vector<char>>& grid) {
+        if(i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size() || grid[i][j] == '0')
+            return;
+        
+        grid[i][j] = '0';
+        
+        dfs(i+1, j, grid);
+        dfs(i, j+1, grid);
+        dfs(i-1, j, grid);
+        dfs(i, j-1, grid);
+    }
+};
