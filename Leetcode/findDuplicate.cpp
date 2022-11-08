f11a06954f653a12da14006c80b9adb3c26bf835
ShivajiReddy <shivajireddy.d@gmail.com>
2022-11-08 11:31:48 -0800

    practice array duplicates and bit manipulation

--- Leetcode/findDuplicate.cpp
+++ Leetcode/findDuplicate.cpp
@@ -1,28 +1,17 @@
 class Solution {
 public:
-    int findDuplicate(vector<int>& nums) {
-        //[1,3,4,2,2]
-        //[0,1,2,3,4]
-        //-1,-3,-4,-2,already -2
-        int ans;
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector<int> res;
+        
         for(int i = 0; i < nums.size(); i++) {
             int index = abs(nums[i]) - 1;
             if(nums[index] < 0) {
-                ans = abs(nums[i]);
-                break;
+                res.push_back(abs(nums[i]));
             }
-            else 
-                nums[index] *= -1;
+            else
+                nums[index] = nums[index]*-1;
         }
         
-        return ans;
-        
-        //[3,1,3,4,2]
-        //[0,1,2,3,4]
-        /*[3,1,-3,4,2]
-          [-3,1,-3,4,2]
-          [-3,1,-3,4,2]
-        
-        */
+        return res;
     }
 };
